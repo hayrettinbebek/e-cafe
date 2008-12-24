e33e0e0122a05d56979dce15959304a70dec5236
laszlo.erno <laszlo.erno@1bf0342c-ceea-11dd-a412-a1792178251d>
2008-12-24 19:23:11 +0000

    cikkválasztó kezdet

--- e_Cafe/UserControls/CikkValaszt.Designer.cs
+++ e_Cafe/UserControls/CikkValaszt.Designer.cs
@@ -43,6 +43,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.Controls.Add(this.lstCikkcsoport);
             this.Name = "CikkValaszt";
             this.Size = new System.Drawing.Size(575, 356);
